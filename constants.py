423563fd5188236020b61ab37e4922674bbb91a6
Tiffany Denny <tiffany@tiffanydenny.com>
2020-07-06 15:40:13 -0500

    Fussing with gitignore

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = '1Bo4WQ8yWiLMxVeeTnh3RmFg6'
+CONSUMER_SECRET = '3EgsmUBklb152T89AvEgwpuRGiCtVbTbheZazfgahoTXTz0djm'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTH_URL = 'https://api.twitter.com/oauth/authorize'
